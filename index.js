81da88d19497ec752ad6362fecfbc98569cbab64
Dannybros <akbaromega89@gmail.com>
2023-09-12 14:14:38 +0700

    testing cors 1.8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import {createServer} from 'http';
+import { createServer } from 'http';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import items from './route/Items.js'
@@ -12,12 +12,23 @@ import order from './route/Order.js'
 import imports from './route/Import.js'
 import review from './route/Review.js'
 import stripe from './route/Stripe.js'
-import {Server} from 'socket.io'
+import { Server } from 'socket.io'
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+
 const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods:["GET", "POST"]
+    },
+});
+
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI,{
@@ -29,19 +40,9 @@ mongoose.connection.on("connected", ()=>{
     console.log("Mongoose is connected");
 })
 
-app.use(cors());
-
-const io = new Server(server, {
-    cors: {
-      origin: "https://pharmacy-client.netlify.app"
-    },
-});
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+server.listen(port, ()=>console.log(`App is starting now in ${port}`));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     req.io = io;
     return next();
 });
@@ -60,18 +61,12 @@ app.get('/', (req, res)=> {
     res.status(200).send("testing cors 1.6");
 });
 
-server.listen(port, ()=>console.log(`App is starting now in ${port}`));
-
 global.onlineUsers = new Map();
 
 io.on('connection', (socket)=>{
-    
     socket.on('User_Online', (userId)=>{
         onlineUsers.set(userId, socket.id)
-        
         console.log(onlineUsers);
-        socket.emit("Test", {message:"Hello"})
+        socket.emit("user_test", "hello");
     })
-
-    console.log(socket.id);
 })
